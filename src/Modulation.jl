06876ee774b73aa54261e278530c1492adbc8c69
ferreol <ferreol.soulez@free.fr>
2023-03-28 18:56:30 +0200

    Fix modulation missing power

--- src/Modulation.jl
+++ src/Modulation.jl
@@ -120,7 +120,7 @@ end
 
 function (self::Chi2CostFunction{T})(b::T,ϕ::T) where{T<:AbstractFloat}
 	pupilmodulation = updatemodulation(self.mod, self.timestamp, self.data, b, ϕ)
-	return sum(abs2,( pupilmodulation .- self.data))
+	return sum(abs2,( self.power .*  pupilmodulation .- self.data))
 end
 
 function (self::Chi2CostFunction{T})(pupilmodulation::AbstractVector{Complex{T}},b::T,ϕ::T) where{T<:AbstractFloat}
